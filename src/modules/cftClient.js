076a59f2ccfe77383b0bdabb7d9c4935dc744bed
Mirasaki <pcr.hillebrand1998@gmail.com>
2022-05-16 11:24:07 +0200

    remove cftoools-sdk withCache call

--- src/modules/cftClient.js
+++ src/modules/cftClient.js
@@ -9,7 +9,6 @@ const {
 
 // export our CFTools client as unnamed default
 module.exports = new cftSDK.CFToolsClientBuilder()
-  .withCache()
   .withServerApiId(CFTOOLS_SERVER_API_ID)
   .withCredentials(CFTOOLS_API_APPLICATION_ID, CFTOOLS_API_SECRET)
   .build();
