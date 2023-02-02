7ea9d2d1c7f0888d96b2917be2f266c57a6ddcf5
Mirasaki <pcr.hillebrand1998@gmail.com>
2023-02-02 12:55:18 +0100

    `/stats` - reduce cognitive complexity

--- src/commands/dayz/stats.js
+++ src/commands/dayz/stats.js
@@ -88,61 +88,68 @@ module.exports = {
       logger.endLog('STAT COMMAND DATA');
     }
 
-    // Assigning our stat variables
-    const { omega, game } = stats;
-    const { general } = game;
-    const daysPlayed = Math.floor(omega.playtime / 86400);
-    const hoursPlayed = Math.floor(omega.playtime / 3600) % 24;
-    const minutesPlayed = Math.floor(omega.playtime / 60) % 60;
-    const secondsPlayed = omega.playtime % 60;
-    const playSessions = omega.sessions;
-    const averagePlaytimePerSession = Math.round(
-      ((hoursPlayed * 60)
-      + minutesPlayed)
-      / playSessions
-    );
-    const [
-      day, month, date, year, time, timezone
-    ] = `${new Date(stats.updated_at)}`.split(' ');
-    let favoriteWeaponName = 'Knife';
-    const highestKills = Object.entries(general?.weapons || {}).reduce((acc, [weaponName, weaponStats]) => {
-      const weaponKillsIsLower = acc > weaponStats.kills;
-      if (!weaponKillsIsLower) favoriteWeaponName = weaponName;
-      return weaponKillsIsLower ? acc : weaponStats.kills;
-    }, 0);
-    const cleanedWeaponName = titleCase(favoriteWeaponName.replace(/_/g, ' '));
-
-    // Reversing the name history array so the latest used name is the first item
-    omega.name_history.reverse();
-
-    // Returning our detailed player information
-    interaction.editReply({
-      embeds: [
-        {
-          color: colorResolver(),
-          title: `Stats for ${omega.name_history[0] || 'Survivor'}`,
-          description: stripIndents`
-            Survivor has played for ${daysPlayed} days, ${hoursPlayed} hours, ${minutesPlayed} minutes, and ${secondsPlayed} seconds - over ${playSessions} total sessions.
-            Bringing them to an average of ${!isNaN(averagePlaytimePerSession) ? averagePlaytimePerSession : 'n/a'} minutes per session.
-
-            **Name History:** **\`${omega.name_history.slice(0, 10).join('`**, **`') || 'None'}\`**
-
-            **Deaths:** ${general?.deaths || 0}
-            **Hits:** ${general?.hits || 0}
-            **KDRatio:** ${general?.kdratio || 0}
-            **Kills:** ${general?.kills || 0}
-            **Longest Kill:** ${general?.longest_kill || 0} m
-            **Longest Shot:** ${general?.longest_shot || 0} m
-            **Suicides:** ${general?.suicides || 0}
-            **Favorite Weapon:** ${cleanedWeaponName || 'Knife'} with ${highestKills || 0} kills
-          `,
-          footer: {
-            text: `Last action: ${time} | ${day} ${month} ${date} ${year} ${time} (${timezone})`
-          }
-        }
-      ]
-    });
+    // Dedicated function for stat calculations
+    // and sending the result to reduce
+    // cognitive complexity
+    sendPlayerData(stats, interaction);
   }
+};
+
+const sendPlayerData = (stats, interaction) => {
+  // Assigning our stat variables
+  const { omega, game } = stats;
+  const { general } = game;
+  const daysPlayed = Math.floor(omega.playtime / 86400);
+  const hoursPlayed = Math.floor(omega.playtime / 3600) % 24;
+  const minutesPlayed = Math.floor(omega.playtime / 60) % 60;
+  const secondsPlayed = omega.playtime % 60;
+  const playSessions = omega.sessions;
+  const averagePlaytimePerSession = Math.round(
+    ((hoursPlayed * 60)
+    + minutesPlayed)
+    / playSessions
+  );
+  const [
+    day, month, date, year, time, timezone
+  ] = `${new Date(stats.updated_at)}`.split(' ');
+  let favoriteWeaponName = 'Knife';
+  const highestKills = Object.entries(general?.weapons || {}).reduce((acc, [weaponName, weaponStats]) => {
+    const weaponKillsIsLower = acc > weaponStats.kills;
+    if (!weaponKillsIsLower) favoriteWeaponName = weaponName;
+    return weaponKillsIsLower ? acc : weaponStats.kills;
+  }, 0);
+  const cleanedWeaponName = titleCase(favoriteWeaponName.replace(/_/g, ' '));
+
+  // Reversing the name history array so the latest used name is the first item
+  omega.name_history.reverse();
+
+  // Returning our detailed player information
+  interaction.editReply({
+    embeds: [
+      {
+        color: colorResolver(),
+        title: `Stats for ${omega.name_history[0] || 'Survivor'}`,
+        description: stripIndents`
+          Survivor has played for ${daysPlayed} days, ${hoursPlayed} hours, ${minutesPlayed} minutes, and ${secondsPlayed} seconds - over ${playSessions} total sessions.
+          Bringing them to an average of ${!isNaN(averagePlaytimePerSession) ? averagePlaytimePerSession : 'n/a'} minutes per session.
+
+          **Name History:** **\`${omega.name_history.slice(0, 10).join('`**, **`') || 'None'}\`**
+
+          **Deaths:** ${general?.deaths || 0}
+          **Hits:** ${general?.hits || 0}
+          **KDRatio:** ${general?.kdratio || 0}
+          **Kills:** ${general?.kills || 0}
+          **Longest Kill:** ${general?.longest_kill || 0} m
+          **Longest Shot:** ${general?.longest_shot || 0} m
+          **Suicides:** ${general?.suicides || 0}
+          **Favorite Weapon:** ${cleanedWeaponName || 'Knife'} with ${highestKills || 0} kills
+        `,
+        footer: {
+          text: `Last action: ${time} | ${day} ${month} ${date} ${year} ${time} (${timezone})`
+        }
+      }
+    ]
+  });
 };
 
 const tryPlayerData = async (client, interaction, identifier, CFTOOLS_SERVER_API_ID) => {
